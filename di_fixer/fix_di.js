c4fc23cbbe0c9f8692a2d0bf022c030ad20f3c26
Torbjörn Mattsson <torbjoern.mattsson@gmail.com>
2016-09-18 15:16:00 +0200

    First working version of di_fixer

---
+++ di_fixer/fix_di.js
@@ -0,0 +1,19 @@
+function remove_paywall()
+{
+    console.log('showing paywall content');
+    a = document.getElementsByClassName("paywall-content")
+    if(a.length != 0){
+        a[0].style = "height:auto"
+    } else {
+        setTimeout(remove_paywall, 1000)
+    }
+}
+
+setTimeout(remove_paywall, 1000)
+
+// remove anoying top banner
+a = document.getElementsByClassName("di_panorama-wrapper")
+if(a.length != 0){
+    a[0].parentNode.removeChild(a[0])
+}
+
